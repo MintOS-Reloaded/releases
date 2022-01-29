cdbdcb3c5ba280895987633668263bca58a1b3dd
SirRGB <sirrgb@amyrom.ml>
2022-01-29 14:47:03 +0100

    initial changes for Mint

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="SirRGB"
+export GITHUB_EMAIL="sirrgb@amyrom.ml"
 
 export device=""
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Mint OS"
+export ROM_DIR="${WORKSPACE}/../Min10"
+export ROM_VERSION="2.0"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/MintOS-Reloaded/Mint-OS/master/99-mintos.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-17.1"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
+export ccache="false"
 export ccache_size=""
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="MintOS-Reloaded/releases"
 
 export timezone="UTC"
